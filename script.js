72b899725be4a2df266bc461d67ade42f4f10144
aleksandrinakim <aleksandrina_kik02@mail.ru>
2020-02-16 02:24:35 +0500

    first commit after finish before adaptive

---
+++ script.js
@@ -0,0 +1,23 @@
+var userName = prompt('Как вас зовут?');
+console.log('Меня' + ' зовут ' + userName);
+
+var userAge = prompt('Сколько вам лет?');
+console.log('Мне ' + userAge + ' лет');
+
+var userComputation1 = prompt('Вычислите: 100 + 100 = ?');
+console.log('Пример' + ' 1' + ': ' + '100 + 100 ' + '=' + ' 200 ' + 'Ваш ответ' + ': ' + userComputation1);
+
+var userComputation2 = prompt('Вычислите: 555 - 111 = ?');
+console.log('Пример' + ' 2' + ': ' + ' 555 - 111 ' + '=' + ' 444 ' + 'Ваш ответ' + ': ' + userComputation2);
+
+var userComputation3 = prompt('Вычислите: 111 * 3 = ?');
+console.log('Пример' + ' 3' + ': ' + '111 * 3 ' + '=' + ' 333 ' + 'Ваш ответ' + ': ' + userComputation3);
+
+var userComputation4 = prompt('Вычислите: 666 / 3 = ?');
+console.log('Пример' + ' 4' + ': ' + '666 / 3 ' + '=' + ' 222 ' + 'Ваш ответ' + ': ' + userComputation4);
+
+var userComputation5 = prompt('Вычислите: 80 % 9 = ?');
+console.log('Пример' + ' 5' + ': ' + '80 / 9 ' + '=' + ' 8 ' + 'Ваш ответ' + ': ' + userComputation5);
+
+
+
